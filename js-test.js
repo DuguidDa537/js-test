9c2120a1273f5dd9c3a88cb59f908f0e5da0270d
DuguidDa537 <DuguidDa537@northwested.org>
2026-04-24 10:03:36 -0400

    Add functions to calculate triangle area

--- js-test.js
+++ js-test.js
@@ -1,3 +1,17 @@
 //Danyl John Duguid
 //4/24/2026
 //Javascript Functions Test
+
+function triangleBase() {
+    return parseFloat(prompt("What is the base of your triangle?\n"));
+}
+
+function triangleHeight() {
+    return parseFloat(prompt("what is the height of your triangle?\n"));
+}
+
+function triangleArea(triangleBase, triangleHeight){
+    return((triangleBase * triangleHeight)/2);
+}
+
+alert("your triangle's area is:" + triangleArea(triangleBase(), triangleHeight()));
